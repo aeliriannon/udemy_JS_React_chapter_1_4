b209b2ad3698eda5ab570346363678eaeb55799a
Aeliriannon <iriska8807@gmail.com>
2021-10-06 16:51:56 +0300

    new lessons

---
+++ lesson_16_функции/js/script.js
@@ -0,0 +1,22 @@
+"use strict";  // используется для обозначения того, что код пишем в новом формате, пишев в верху главного документа
+
+//Функции
+
+function showFirstMessage(text) {
+    console.log(text);
+}
+showFirstMessage('Hello world!')
+
+function calc(a, b) {
+    return (a + b);   // return --> вернуть
+}
+
+console.log(calc(3, 5));
+
+const logger = function() {
+    console.log('Hello');
+};
+
+logger();
+// стрелочная функция
+const calc = (a, b) => a+b
