686e74a95618cbaeb4231ca9d061584092c7defb
Aeliriannon <iriska8807@gmail.com>
2022-01-22 18:29:51 +0300

    added lesson 38 - practice

--- lesson_chapter_3/lesson_37_classList_и_делегирование_событий/js/main.js
+++ lesson_chapter_3/lesson_37_classList_и_делегирование_событий/js/main.js
@@ -1,1 +1,82 @@
 'use strict';
+
+const btns = document.querySelectorAll('button'),
+    wrapper = document.querySelector('.btn-block');
+
+// console.log(btns[0].classList.length); // узнаем сколько классов у первой кнопки
+
+// console.log(btns[0].classList.item(0)); //узнаем какой класс у элемента под нулевым индексом
+
+// console.log(btns[1].classList.add('red', 'new_class'));  //добавояет класс
+// console.log(btns[0].classList.remove('blue')); //удаляет класс
+// console.log(btns[0].classList.toggle('blue')); //проверяет, если класс есть, то удаляет, если нету то добавляет
+
+//---!!!Устарело ---
+
+// console.log(btns[0].className );
+
+
+// btns[0].addEventListener('click', () => {  //назнгачаем на первую кнопку отслеживание клика
+// if(!btns[1].classList.contains('red')) { //если у второй кнопки нет класса red то   
+//     btns[1].classList.add('red'); //то добавляем этот класс 
+// } else  {
+//     btns[1].classList.remove('red'); //иначе удаляем этот класс
+// }
+
+//     btns[1].classList.toggle('red'); //делает то же самое, что и условный оператор выше
+
+// });
+
+
+//--- Делегирование событитий ---
+
+
+
+// wrapper.addEventListener('click', (event) => {  //говорим, что будем отслеживать клик на родителе наших кнопок
+//     //передаем во внутрь объект событития
+//         // console.dir(event.target); //вызываем target в качестве объекта в консоли
+
+//         if (event.target && event.target.classList.contains('blue')) { //проверяем, существует ли вообще свойство target у элемента(возможность клика) и есть ли у элемнта класс blue
+//             console.log('Hello');
+
+//         }
+
+//     });
+
+// wrapper.addEventListener('click', (event) => {  //говорим, что будем отслеживать клик на родителе наших кнопок
+// //передаем во внутрь объект событития
+//     // console.dir(event.target); //вызываем target в качестве объекта в консоли
+
+//     if (event.target && event.target.tagName == 'BUTTON') { //проверяем, существует ли вообще свойство target у элемента(возможность клика) и  сравниваем имя тэга
+//         console.log('Hello');
+
+//     }
+
+// });
+
+//--- mathes сравнивает что-либо с чем-либо---
+
+wrapper.addEventListener('click', (event) => { //говорим, что будем отслеживать клик на родителе наших кнопок
+    //передаем во внутрь объект событития
+    // console.dir(event.target); //вызываем target в качестве объекта в консоли
+
+    if (event.target && event.target.matches('button.red')) {
+        //проверяем, существует ли вообще свойство target у элемента(возможность клика) и сравниваем будет ли это элемент button  с классом .red            console.log('Hello');
+        console.log('Hello');
+    }
+
+});
+
+//--- НЕ используем деллигировоание и не видим динамическую кнопку созданную позже, чем этот скрипт
+
+// btns.forEach(btn => { //перебираем псевдомассив из кнопок, берем каждую кнопку
+//     btn.addEventListener('click', () => { //отслеживаем клик на каждую кнопку
+//         console.log('Hello');
+//     });
+// });
+
+
+
+const btn = document.createElement('button'); //создаем новые кнопки
+btn.classList.add('red'); //добавляем динамической кнопке класс red
+wrapper.append(btn); //добавляем кнопку в конец элемента враппер
---
+++ practice_udemy/chapter_3/Food_38/js/main.js
@@ -0,0 +1,60 @@
+'use strict';
+
+window.addEventListener('DOMContentLoaded', () => {  //отслеживаем загрузку контента на странице
+
+//--- Создаем переменные ---
+
+    const tabs = document.querySelectorAll('.tabheader__item'), //получаем псевдомассив табов из меню
+        tabsContent = document.querySelectorAll('.tabcontent'), //получаем псевдомассив блоков с контентом табов
+        tabsParent = document.querySelector('.tabheader__items'); // получаем родителя всех табов из меню
+
+        console.log(tabs);
+        console.log(tabsContent);
+//--- Скрываем ненужный контент для табов и убираем класс active с самих табов ---
+    
+    function hideTabContent() {
+        tabsContent.forEach(item => {  //перебираем псевдомассив блоков с контентом, берем каждый отдельно
+            // item.style.display = 'none'; //обращаемся к свойству style и прописываем ему в свойство display значение none(инлайновые стили)
+            item.classList.remove('show', 'fade');
+            item.classList.add('hide');
+        });
+
+        tabs.forEach(item => {  //перебираем псевдомассив табов
+            item.classList.remove('tabheader__item_active'); //берем каждый таб, обращаемся к объекту класслист и задаем метод ремув - удаляем указанный класс
+        });
+    }
+
+//--- Показываем нужный контент для табов и добавляем класс active на нужный нам тьаб ---
+
+    function showTabContent(i = 0) { //стандарт es6, если не передается аргумент, то подставится 0
+        // tabsContent[i].style.display = 'block'; //прописываем для конкретного блока с контентом инлайновый стиль дисплей блок
+        tabsContent[i].classList.remove('hide');
+        tabsContent[i].classList.add('show', 'fade');
+        tabs[i].classList.add('tabheader__item_active'); //для конкретнгого таба добавляем класс активности
+    }
+
+//--- вызываем функции ---
+
+    hideTabContent();
+    showTabContent();
+
+//--- используем делегирование, отслеживаем клик на один из табов, задаем ему классактивности  и открываем нужный таб ---
+
+    tabsParent.addEventListener('click', (event) => { //отслеживаем клик в родителе табов
+        const target = event.target; //создаем переменную с объектом таргет
+
+        if (target && target.classList.contains('tabheader__item')) { //проверяем есть ли вообще объект таргет у события и проверяем соответствие класса, по тому ли элементу мы кликнули
+            tabs.forEach((item, i) => { //перебираем псевдомассив всех табов, берем таб и его индекс
+                if (target == item) { //если объект таргет равен нашему данному элементу
+                    hideTabContent();
+                    showTabContent(i); //переключаем табы
+                }
+
+            });
+
+        }
+
+    });
+
+
+});
